8c40990e2e2129cedd3ad2f4d215b4e60f925116
Bachdble <bachdble@users.noreply.github.com>
2015-03-14 17:04:04 -0600

    Use double pointers instead of single for card deck.

--- include/models/carddeck.h
+++ include/models/carddeck.h
@@ -4,59 +4,61 @@
 #include "models/cardset.h"
 
 // cardSet dealerDeck[MAX_CARD_TYPES];
-cardSet *dealerDeck;
+cardSet **dealerDeck;
 
 /**
  * Initializes the card deck.
  *
- * @param cardSet* deck The deck.
+ * @param int numCards The number of cards of each type.
+ *
+ * @return cardSet** deck The deck.
  */
-cardSet* CreateDeck();
+cardSet** CreateDeck(int numCards);
 
 /**
  * Removes and returns a random card from the deck.
  *
- * @param cardSet* deck The deck.
+ * @param cardSet** deck The deck.
  *
  * @return A random cardType from the deck.
  */
-cardType GetRandomCardFromDeck(cardSet* deck);
+cardType GetRandomCardFromDeck(cardSet** deck);
 
 /**
  * Add count cards of cardType type to the deck.
  *
- * @param cardSet* deck The deck.
+ * @param cardSet** deck The deck.
  * @param cardType ct A card type.
  * @param int count The number of cards to add.
  */
-void AddCardsToDeck (cardSet* deck, cardType ct, int count);
+void AddCardsToDeck (cardSet** deck, cardType ct, int count);
 
 /**
  * Removes all of the cards of type cardType from the deck and
  *
- * @param cardSet* deck The deck.
+ * @param cardSet** deck The deck.
  * @param cardType ct The type of card to remove.
  *
  * @return The number removed.
  */
-int RemoveCardsFromDeck (cardSet* deck, cardType ct);
+int RemoveCardsFromDeck (cardSet** deck, cardType ct);
 
 /**
  * Returns true if the deck contains at least one of cardType, false otherwise.
  *
- * @param cardSet* deck The deck.
+ * @param cardSet** deck The deck.
  * @param cardType ct The card to check for.
  *
  * @return 1 if the card exists, 0 otherwise
  */
-int DeckHasType (cardSet* deck, cardType ct);
+int DeckHasType (cardSet** deck, cardType ct);
 
 /**
  * Frees the memory of a previously allocated a cardSet*
  *
- * @param cardSet* deck The Deck to free.
+ * @param cardSet** deck The Deck to free.
  *
  */
-void DestructDeck (cardSet* deck);
+void DestructDeck (cardSet** deck);
 
 #endif
--- src/models/carddeck.c
+++ src/models/carddeck.c
@@ -1,57 +1,63 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include "models/carddeck.h"
 
-cardSet* CreateDeck() {
-	cardSet *tempDeck = malloc (sizeof(cardSet)*MAX_CARD_TYPES);
-	for(cardType i = 0; i < MAX_CARD_TYPES; i++) {
-		SetCardType (&tempDeck[i], i);
-		AddCards(&tempDeck[i], MAX_CARDS_PER_SET);
-	}
+cardSet** CreateDeck(int numCards) {
+	cardSet **tempDeck = NULL;
 
-	srand(time(NULL));
+	if (numCards > 0 && numCards <= MAX_CARDS_PER_SET) {
+		tempDeck = malloc (sizeof(cardSet *)*MAX_CARD_TYPES);
+		for(cardType i = 0; i < MAX_CARD_TYPES; i++) {
+			tempDeck[i] = malloc (sizeof(cardSet));
+			SetCardType (tempDeck[i], i);
+			AddCards(tempDeck[i], numCards);
+		}
+
+		srand(time(NULL));
+	}
 
 	return tempDeck;
 }
 
-cardType GetRandomCardFromDeck(cardSet* deck) {
+cardType GetRandomCardFromDeck(cardSet** deck) {
 	cardType randType = 0;
 
 	do {
 		randType = rand() % MAX_CARD_TYPES;
-	} while (GetNumCards(&deck[randType]) <= 0);
+	} while (GetNumCards(deck[randType]) <= 0);
 
-	RemoveCards(&deck[randType],1);
+	RemoveCards(deck[randType],1);
 
 	return randType;
 }
 
-void AddCardsToDeck (cardSet* deck, cardType ct, int count) {
+void AddCardsToDeck (cardSet** deck, cardType ct, int count) {
 	if(ct < MAX_CARD_TYPES) {
 		if (count > 0 && count < MAX_CARDS_PER_SET) {
-			if ((GetNumCards(&deck[ct]) + count) < MAX_CARDS_PER_SET) {
-				AddCards(&deck[ct], count);
+			if ((GetNumCards(deck[ct]) + count) < MAX_CARDS_PER_SET) {
+				AddCards(deck[ct], count);
 			}
 		}
 	}
 }
 
-int RemoveCardsFromDeck (cardSet* deck, cardType ct) {
+int RemoveCardsFromDeck (cardSet** deck, cardType ct) {
 	int countRemoved = 0;
 
 	if(ct < MAX_CARD_TYPES) {
-		countRemoved = GetNumCards(&deck[ct]);
-		RemoveCards(&deck[ct],countRemoved);
+		countRemoved = GetNumCards(deck[ct]);
+		RemoveCards(deck[ct],countRemoved);
 	}
 
 	return countRemoved;
 }
 
-int DeckHasType (cardSet* deck, cardType ct) {
+int DeckHasType (cardSet** deck, cardType ct) {
 	int hasType = 0;
 
 	if(ct < MAX_CARD_TYPES) {
-		if(GetNumCards(&deck[ct]) > 0) {
+		if(GetNumCards(deck[ct]) > 0) {
 			hasType = 1;
 		}
 	}
@@ -59,6 +65,11 @@ int DeckHasType (cardSet* deck, cardType ct) {
 	return hasType;
 }
 
-void DestructDeck (cardSet *deck) {
+void DestructDeck (cardSet **deck) {
+	for(cardType i = 0; i < MAX_CARD_TYPES; i++) {
+		free(deck[i]);
+		deck[i] = NULL;
+	}
 	free (deck);
+	deck = NULL;
 }
